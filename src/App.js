91b34d4cac6c73c6ac7741708cb039e1d1e4e885
Dirk <dibo@telenet.be>
2020-03-24 18:05:07 +0100

    sign in component, custom buttom component and form input component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+
+// import Pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header-component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+// import Components
+import Header from './components/header/header-component';
 
 function App() {
   return (
@@ -14,6 +18,7 @@ function App() {
       <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route exact={true} path='/shop' component={ShopPage} />
+          <Route exact={true} path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       
     </div>
